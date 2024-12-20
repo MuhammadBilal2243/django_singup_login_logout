4328460ecdbe54281728640e85b8492438d2e323
MuhammadBilal2243 <bilal0302599@gmail.com>
2024-12-20 16:20:17 +0500

    adding more feature

--- pr01/acounts/urls.py
+++ pr01/acounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index,name = 'index'),
+    path('registeruser', views.registeruser,name = 'registeruser'),
+    path('loginuser', views.loginuser,name = 'loginuser'),
+    path('logoutuser', views.logoutuser,name = 'logoutuser'),
+
 ]
--- pr01/acounts/views.py
+++ pr01/acounts/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import RegisterUserForm
+#imports for login
+from .forms import LoginUserForm
+from django.contrib.auth import login,logout,authenticate
 # Create your views here.
 def index(request):
     return render(request,"acounts/index.html")
+def registeruser(request):
+    
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print("for is submited")
+            return HttpResponse("user is crated")
+    else:
+        form = RegisterUserForm()
+    context={"form":form }
+    return render(request,"acounts/registeruser.html" ,context)
+
+
+def logoutuser(request):
+    logout(request)
+    return HttpResponse("you log out good its working")
+
+
+
+
+
+def loginuser(request):
+ 
+    if request.method=='POST':
+            
+            username=request.POST["username"]
+            password=request.POST["password"]
+            
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponse("congrates u login")
+            else:
+                return HttpResponse("invilid user name or password")
+        
+    else:
+        form = LoginUserForm()
+    
+        
+    context={"form":form}
+    return render(request,"acounts/loginuser.html",context)
